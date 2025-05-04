31599bdbda796754fc368f9a6f4d39d1a9c2acb9
Mutsuzhayat45 <officialmutsuz@gmail.com>
2025-05-04 12:46:02 +0300

    Create MutsuzScripts.lua
    This script contains Lumber tycoon 2 script. Producer: MutsuzScripts

---
+++ MutsuzScripts.lua
@@ -0,0 +1,4 @@
+-- Lumber Tycoon 2 GUI by Mutsuz_hayat45
+-- Para, ağaç tp, araba tp, auto build gibi her şey içerir.
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/LumberTycoon2Full"))()
